25009bb333c0bbfdcbe222fc0cecf027767f1e22
builtauthentic <74743864+builtauthentic@users.noreply.github.com>
2020-11-22 22:45:31 -0600

    Moved grading scale to global variables

--- passFail.go
+++ passFail.go
@@ -12,6 +12,10 @@ import (
 //declaring global variables
 var input string
 var grade float64
+var gradeA float64 = 94
+var gradeB float64 = 86
+var gradeC float64 = 74
+var gradeD float64 = 65
 
 func pF(){
   //order of functions
@@ -40,10 +44,16 @@ func gradeInput (){
 
 func rubric(gr float64){
   //compare grade entered to grading scale
-  if gr >= 60{
-    fmt.Println("Congratulations! You passed.")
+  if gr >= gradeA{
+    fmt.Println("Congratulations! You got an A.")
+  } else if gr >= gradeB{
+    fmt.Printf("Not bad. You got a B and were only %.2f percentage points away from an A.\n",gradeA-grade)
+  } else if gr >= gradeC{
+    fmt.Printf("Meh. You got a C and were only %.2f percentage points away from a B.\n",gradeB-grade)
+  } else if gr >= gradeD{
+    fmt.Printf("Better luck next time. You were only %.2f percentage points away from a C.\n",gradeC-grade)
   } else {
-    fmt.Printf("Better luck next time. You were only %.2f percentage points away from passing.\n",60-grade)
+    fmt.Printf("Congratulations! Despite being %.2f percentage points away from passing, you've received the greatest gift of all: Failure.\n",gradeD-grade)
   }
 }
 

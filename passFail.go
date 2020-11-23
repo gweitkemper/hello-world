ca3e649f0d0a0e745bdf14177ff7ebfad17ea55d
builtauthentic <74743864+builtauthentic@users.noreply.github.com>
2020-11-22 22:12:28 -0600

    Quick program to grasp modularizing functions
    This program allows a  user to enter one or more grades and have them compared against a fixed grading scale.

---
+++ passFail.go
@@ -0,0 +1,62 @@
+//This program compares an entered grade to a graing scale and displays the result
+package main
+import (
+  "fmt"
+  "log"
+  "strings"
+  "strconv"
+  "os"
+  "bufio"
+)
+
+var input string
+var grade float64
+
+func main(){
+  //create a new reader
+  gradeInput()
+  rubric(grade)
+  rerun()
+}
+
+func gradeInput (){
+  //capture user input of grade
+  fmt.Print("What grade did you receive?  ")
+  reader := bufio.NewReader(os.Stdin)
+  input, err := reader.ReadString('\n')
+    if err != nil {
+      fmt.Println("ERROR:")
+      log.Fatal(err)
+    }
+  input = strings.TrimSpace(input)
+  //identify captured input and convert to float for comparison
+  grade, err = strconv.ParseFloat(input, 64)
+    if err != nil {
+      fmt.Println("ERROR:")
+      log.Fatal(err)
+    }
+  }
+
+func rubric(gr float64){
+  if gr >= 60{
+    fmt.Println("Congratulations! You passed.")
+  } else {
+    fmt.Printf("Better luck next time. You were only %.2f percentage points away from passing.\n",60-grade)
+  }
+}
+
+func rerun(){
+  fmt.Println("Would you like to enter another grade? (y/n)")
+  reader := bufio.NewReader(os.Stdin)
+  input, err := reader.ReadString('\n')
+    if err != nil {
+      fmt.Println("ERROR:")
+      log.Fatal(err)
+    }
+    input = strings.TrimSpace(input)
+    if input == "y"{
+      main()
+    } else {
+      fmt.Println("Thanks for using passFail.go!")
+    }
+}
